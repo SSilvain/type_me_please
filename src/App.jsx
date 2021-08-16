4718f09c174b32770b17b47755cc8b02b0e2565e
Silvain <silver383@gmail.com>
2021-08-16 16:17:00 +0300

    useEffect all symbols

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Sentence from './Components/Sentence/Sentence'
-import Word from './Components/Word/Word'
 
 
 
 function App() {
-	const [typeMe, setTypeMe] = useState("")
-	const [startCount, setStartCount] = useState(true)
-	let [typingStart, setTypingStart] = useState(0)
-	const typingText = ["Read", "the", "latest,", "technology", "news", "and", "interesting."]
-	let [errorTyping, setErrorTyping] = useState(false)
 
-
-	const typingTextWithSpaces = typingText.map((word, index, arr) => {
-		if (index < (arr.length - 1)) {
-			return word + "_"
-		} else {
-			return word
-		}
-	})
-
-	const coloredText = () => {
-
-		return typingTextWithSpaces[0].slice(0, typeMe.length)
-
-	}
-	const coloredText2 = () => {
-
-		return typingTextWithSpaces[0].slice(typeMe.length)
-
-	}
-
-	const startType = (e) => {
-		if (startCount) {
-			setTypingStart(Date.now())
-			setStartCount(false)
-		}
-		setTypeMe(e.target.value)
-	}
 
 	return (
 		<div>
 			<h1>Type me, please!</h1>
-			<h1>{typingTextWithSpaces}</h1>
-			<span style={errorTyping ? { color: "red" } : { color: "blue" }}>{coloredText()}</span>
-			<span style={{ color: "#444" }}>{coloredText2()}</span>
-			<p>{typingText}</p>
+
 			<Sentence
-				setErrorTyping={setErrorTyping}
-				typingText={typingTextWithSpaces[0]}
-				typingStart={typingStart}
-				setTypingStart={setTypingStart}
-				typeMe={typeMe}
-				setStartCount={setStartCount}
-				setTypeMe={setTypeMe}
 			/>
-			<p>{typeMe || "typing"}</p>
-			<input style={errorTyping ? { color: "red" } : { color: "green" }} type="text" onChange={startType} placeholder="type_me_please" value={typeMe} />
+
 		</div >
 	)
 }
--- src/Components/Sentence/Sentence.jsx
+++ src/Components/Sentence/Sentence.jsx
@@ -1,9 +1,87 @@
+import { useEffect, useState } from "react"
 import Word from "../Word/Word"
 
-const Sentence = (props) => {
-	return (
 
-		<Word {...props} />
+// const typingText = ["Read", "the", "latest,", "technology", "news", "and", "interesting."]
+const typingText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, aspernatur!"
+let allTextSymbols = typingText.split("")
+// console.log(allTextSymbols)
+let allTextSymbolsScores = []
+let currentSymbols = []
+
+const Sentence = () => {
+	const [typeMe, setTypeMe] = useState("")
+	const [typeMeFullText, setTypeMeFullText] = useState("")
+	const [currentSymbol, setCurrentSymbol] = useState("")
+	const [startCount, setStartCount] = useState(true)
+	let [typingStart, setTypingStart] = useState(0)
+	let [errorTyping, setErrorTyping] = useState(false)
+	let [indexOfSymbol, setIndexOfSymbol] = useState(0)
+
+
+
+	useEffect(() => {
+		if (!startCount && allTextSymbols[indexOfSymbol] === currentSymbol) {
+			if (currentSymbol === " ") {
+				setTypeMe("")
+			}
+			currentSymbols.push(currentSymbol)
+			allTextSymbolsScores.push([currentSymbol, Date.now()])
+			console.log(allTextSymbolsScores)
+			setIndexOfSymbol(indexOfSymbol + 1)
+
+			setErrorTyping(false)
+		} else if (allTextSymbols[indexOfSymbol] !== currentSymbol) {
+			setErrorTyping(true)
+		}
+
+	}, [currentSymbol])
+
+	useEffect(() => {
+		if (!startCount && typingText.startsWith(typeMeFullText)) {
+			setErrorTyping(false)
+			console.log("errorrno")
+		}
+	}, [typeMeFullText])
+	// const typingTextWithSpaces = typingText.map((word, index, arr) => {
+	// 	if (index < (arr.length - 1)) {
+	// 		return word + "_"
+	// 	} else {
+	// 		return word
+	// 	}
+	// })
+
+	// const coloredText = () => {
+
+	// 	return typingTextWithSpaces[0].slice(0, typeMe.length)
+
+	// }
+	// const coloredText2 = () => {
+
+	// 	return typingTextWithSpaces[0].slice(typeMe.length)
+
+	// }
+
+	const startType = (e) => {
+		if (startCount) {
+			setTypingStart(Date.now())
+			setStartCount(false)
+		}
+		setTypeMeFullText(e.target.value)
+		setCurrentSymbol(e.nativeEvent.data)
+		console.log(e.nativeEvent.data)
+
+	}
+
+
+
+	// let allWords = typingText.map((word) => <Word typingText={word} />)
+	return (
+		<>
+			<h1>{typingText}</h1>
+			<input style={errorTyping ? { color: "red" } : { color: "green" }} type="text" onChange={startType} placeholder="type_me_please" value={typeMeFullText} />
+		</>
+
 
 	)
 }
