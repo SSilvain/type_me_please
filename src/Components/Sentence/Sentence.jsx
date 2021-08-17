d1160b26e42c726483a458d51b6a4c1c73f47994
Silvain <silver383@gmail.com>
2021-08-17 10:40:25 +0300

    allText

--- src/Components/Sentence/Sentence.jsx
+++ src/Components/Sentence/Sentence.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react"
-import Word from "../Word/Word"
+import "./Sentence.css"
 
 
 // const typingText = ["Read", "the", "latest,", "technology", "news", "and", "interesting."]
-const typingText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, aspernatur!"
+const typingText = "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
 let allTextSymbols = typingText.split("")
 // console.log(allTextSymbols)
 let allTextSymbolsScores = []
 let currentSymbols = []
+let tmpWord = "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem"
 
 const Sentence = () => {
 	const [typeMe, setTypeMe] = useState("")
@@ -17,32 +18,49 @@ const Sentence = () => {
 	let [typingStart, setTypingStart] = useState(0)
 	let [errorTyping, setErrorTyping] = useState(false)
 	let [indexOfSymbol, setIndexOfSymbol] = useState(0)
+	let [resultArray, setResultArray] = useState([])
 
 
 
 	useEffect(() => {
-		if (!startCount && allTextSymbols[indexOfSymbol] === currentSymbol) {
-			if (currentSymbol === " ") {
-				setTypeMe("")
-			}
-			currentSymbols.push(currentSymbol)
-			allTextSymbolsScores.push([currentSymbol, Date.now()])
-			console.log(allTextSymbolsScores)
-			setIndexOfSymbol(indexOfSymbol + 1)
 
-			setErrorTyping(false)
-		} else if (allTextSymbols[indexOfSymbol] !== currentSymbol) {
-			setErrorTyping(true)
-		}
 
 	}, [currentSymbol])
 
-	useEffect(() => {
-		if (!startCount && typingText.startsWith(typeMeFullText)) {
-			setErrorTyping(false)
-			console.log("errorrno")
+	const baseFunc = () => {
+		if (currentSymbol === " ") {
+			console.log("in")
+			setTypeMe("")
+
+			tmpWord = typingText.slice(indexOfSymbol)
+
 		}
-	}, [typeMeFullText])
+
+		allTextSymbolsScores.push([currentSymbol, Date.now()])
+		setIndexOfSymbol(indexOfSymbol + 1)
+	}
+
+	useEffect(() => {
+		// console.log("tmpWord ", tmpWord)
+		// console.log("typeMe ", typeMe)
+		if (tmpWord.startsWith(typeMe)) {
+			setErrorTyping(false)
+		} else {
+			setErrorTyping(true)
+		}
+		result()
+	}, [typeMe])
+
+	// useEffect(() => {
+	// 	if (!startCount && typingText.slice(indexOfSymbol).startsWith(typeMe)) {//
+
+	// 		setErrorTyping(false)
+	// 		console.log(typingText.slice(indexOfSymbol + 1))
+	// 	}
+	// }, [currentSymbol])
+
+
+
 	// const typingTextWithSpaces = typingText.map((word, index, arr) => {
 	// 	if (index < (arr.length - 1)) {
 	// 		return word + "_"
@@ -67,11 +85,40 @@ const Sentence = () => {
 			setTypingStart(Date.now())
 			setStartCount(false)
 		}
-		setTypeMeFullText(e.target.value)
+		setTypeMe(e.target.value)
 		setCurrentSymbol(e.nativeEvent.data)
-		console.log(e.nativeEvent.data)
+		// console.log(e.target.value)
 
 	}
+	let mapResult = new Map()
+	const result = () => {
+		if (allTextSymbolsScores.length !== 0) {
+			allTextSymbolsScores.reduce((prev, current, index, arr) => {
+				let pair = prev[0] + current[0]
+				let pairTime = current[1] - prev[1]
+				if (prev) {
+					if (mapResult.has(pair)) {
+						let pairTimeSum = (mapResult.get(pair) + pairTime) / 2
+						mapResult.set(pair, (pairTimeSum))
+					} else {
+						mapResult.set(pair, (pairTime))
+					}
+
+				}
+				return current
+			})
+		}
+
+		// console.log("This is map ", mapResult)
+		const mapSort2 = new Map([...mapResult.entries()].sort((a, b) => b[1] - a[1]));
+		let resultArrayTmp = []
+		for (let [key, value] of mapSort2) {
+			resultArrayTmp.push([key, value])
+		}
+		setResultArray(resultArrayTmp)
+	}
+
+	const resultOut = resultArray.map((res, index) => <div key={index}><span className="result">{res[0]}</span> <span className="result">{res[1]}</span></div>)
 
 
 
@@ -79,7 +126,8 @@ const Sentence = () => {
 	return (
 		<>
 			<h1>{typingText}</h1>
-			<input style={errorTyping ? { color: "red" } : { color: "green" }} type="text" onChange={startType} placeholder="type_me_please" value={typeMeFullText} />
+			<input style={errorTyping ? { color: "red" } : { color: "green" }} type="text" onChange={startType} placeholder="type_me_please" value={typeMe} />
+			<div>{resultOut}</div>
 		</>
 
 
